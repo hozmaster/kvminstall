2e7be63ffc535818a66ad5ac4687a0181d71ca8a
Jason Callaway <jason@jasoncallaway.com>
2015-08-10 17:02:25 -0400

    setting up package

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'kvm-install',
+  packages = ['kvm-install'],
+  version = '0.1',
+  description = 'Python helper script for virt-install(1)',
+  author = 'Jason Callaway',
+  author_email = 'jason@jasoncallaway.com',
+  url = 'https://github.com/jason-callaway/kvm-install',
+  download_url = 'https://github.com/jason-callaway/kvm-install/tarball/0.1',
+  keywords = ['kvm', 'qemu', 'vm', 'virt-install'],
+  classifiers = [],
+)
